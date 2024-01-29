7d36652e9bda30fb33a32153d57661adee67d0a7
Sergey Videtskikh <videtskikh@gmail.com>
2024-01-29 16:56:23 +0300

    refactoring colors

--- app/src/main/java/ru/vsv/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/MediaLibraryActivity.kt
@@ -7,7 +7,5 @@ class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_library)
-
-
     }
 }
