49a580165cbea85b029844c01819d470bb797468
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-01 13:19:39 +0300

    Домашнее задание Спринт 7 (#1)
    * add activity_main.xml
    
    * add activity_settings.xml
    
    * add listeners to MainActivity
    
    * add intents
    
    * refactoring colors
    
    * add header to main activity
    
    * add onBackPressed
    
    * refactoring activity_main.xml
    
    * correct activity settings
    
    * bugfix activity_main.xml

--- app/src/main/java/ru/vsv/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/MainActivity.kt
@@ -1,11 +1,31 @@
 package ru.vsv.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        val mediaButton = findViewById<Button>(R.id.media_library_button)
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        val settingButton = findViewById<Button>(R.id.settings_button)
+        settingButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
