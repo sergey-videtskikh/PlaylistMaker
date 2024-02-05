6a1fbab0a9e37607f03b175566887b33037f3fe9
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-05 12:30:33 +0300

    add write to support action to SettingsActivity

--- app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
@@ -11,18 +11,27 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backImage = findViewById<ImageView>(R.id.back_image)
-
         backImage.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
         val shareImage = findViewById<ImageView>(R.id.share_image)
-
         shareImage.setOnClickListener {
-            val sendIntent = Intent().apply {
-                action = Intent.ACTION_SEND
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
                 putExtra(Intent.EXTRA_TEXT, getString(R.string.android_practicum_course_link))
                 type = "text/plain"
+            }
+
+            startActivity(sendIntent)
+        }
+
+        val supportImage = findViewById<ImageView>(R.id.support_image)
+        supportImage.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject_to_support))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_message_to_support))
+                type = "text/plain";
             }
 
             startActivity(sendIntent)
