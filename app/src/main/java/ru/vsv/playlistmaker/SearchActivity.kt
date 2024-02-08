0218e3342957c9a53ca41290c4091efdc0cef8e6
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-08 17:05:18 +0300

    add logic with editText to SearchActivity

--- app/src/main/java/ru/vsv/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/SearchActivity.kt
@@ -1,11 +1,54 @@
 package ru.vsv.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private val TAG = "MyActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backImage = findViewById<ImageView>(R.id.back_image)
+        backImage.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.ic_clear_image_view)
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+        val textWatcherEditText = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.i(TAG, "Введеное значение: $s")
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        searchEditText.addTextChangedListener(textWatcherEditText)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package ru.vsv.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
