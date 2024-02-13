a709fb6aee44f2fca4dae692c1b14345cb303952
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-13 13:49:43 +0300

    update intents

--- app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/SettingsActivity.kt
@@ -3,13 +3,13 @@ package ru.vsv.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
     companion object {
         const val MIME_TYPE_TEXT_PLAIN = "text/plain"
+        const val MIME_TYPE_MESSAGE_RFC822 = "message/rfc822"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,32 +23,31 @@ class SettingsActivity : AppCompatActivity() {
 
         val shareImage = findViewById<ImageView>(R.id.share_image)
         shareImage.setOnClickListener {
-            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
                 putExtra(Intent.EXTRA_TEXT, getString(R.string.android_practicum_course_link))
                 type = MIME_TYPE_TEXT_PLAIN
             }
-
-            startActivity(sendIntent)
+            startActivity(Intent.createChooser(shareIntent, null))
         }
 
         val supportImage = findViewById<ImageView>(R.id.support_image)
         supportImage.setOnClickListener {
-            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+            val supportIntent = Intent(Intent.ACTION_SEND).apply {
                 putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
                 putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject_to_support))
                 putExtra(Intent.EXTRA_TEXT, getString(R.string.email_message_to_support))
-                type = MIME_TYPE_TEXT_PLAIN;
+                type = MIME_TYPE_MESSAGE_RFC822
             }
-            startActivity(sendIntent)
+            startActivity(Intent.createChooser(supportIntent, null))
         }
 
         val agreementImage = findViewById<ImageView>(R.id.agreement_image)
         agreementImage.setOnClickListener {
-            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+            val agreementIntent = Intent(Intent.ACTION_SEND).apply {
                 putExtra(Intent.EXTRA_TEXT, getString(R.string.legal_practicum_offer_link))
                 type = MIME_TYPE_TEXT_PLAIN;
             }
-            startActivity(sendIntent)
+            startActivity(agreementIntent)
         }
     }
 }
