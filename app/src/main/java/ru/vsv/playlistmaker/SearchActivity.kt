b15189484cfce26aac58f3577bb7ccf0053504f1
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-13 14:30:42 +0300

    update activity search

--- app/src/main/java/ru/vsv/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/SearchActivity.kt
@@ -5,35 +5,52 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class SearchActivity : AppCompatActivity() {
 
-    private val TAG = "MyActivity"
+    companion object {
+        private const val LOG_TAG = "SearchActivity"
+        private const val EDIT_TEXT_VIEW_KEY = "EDIT_TEXT_VIEW_KEY"
+    }
+
+    private lateinit var searchEditText: EditText
+
+    private var savedValue: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        savedValue = savedInstanceState?.getString(EDIT_TEXT_VIEW_KEY)
 
         val backImage = findViewById<ImageView>(R.id.back_image)
         backImage.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        searchEditText = findViewById(R.id.search_edit_text)
+        searchEditText.setText(savedValue)
+
         val clearButton = findViewById<ImageView>(R.id.ic_clear_image_view)
 
         clearButton.setOnClickListener {
-            searchEditText.setText("")
+            savedValue = ""
+            searchEditText.setText(savedValue)
+            searchEditText.clearFocus()
+            hideDefaultKeyboard(searchEditText)
         }
 
         val textWatcherEditText = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.i(TAG, "Введеное значение: $s")
+                savedValue = s.toString()
+                Log.i(LOG_TAG, "Введеное значение: $savedValue")
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
@@ -41,7 +58,16 @@ class SearchActivity : AppCompatActivity() {
         }
 
         searchEditText.addTextChangedListener(textWatcherEditText)
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(EDIT_TEXT_VIEW_KEY, savedValue)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        savedValue = savedInstanceState.getString(EDIT_TEXT_VIEW_KEY)
+        searchEditText.setText(savedValue)
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -50,5 +76,10 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun hideDefaultKeyboard(editText: EditText) {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(editText.windowToken, 0)
     }
 }
