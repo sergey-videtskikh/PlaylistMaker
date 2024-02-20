1c20cf98c428b76b933d13bdcf180e619774e931
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-20 16:02:44 +0300

    create search recycler view

---
+++ app/src/main/java/ru/vsv/playlistmaker/dto/Track.kt
@@ -0,0 +1,8 @@
+package ru.vsv.playlistmaker.dto
+
+data class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String,
+)
---
+++ app/src/main/java/ru/vsv/playlistmaker/search/SearchActivity.kt
@@ -0,0 +1,141 @@
+package ru.vsv.playlistmaker.search
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.color.MaterialColors
+import ru.vsv.playlistmaker.R
+import ru.vsv.playlistmaker.dto.Track
+
+
+class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        private const val LOG_TAG = "SearchActivity"
+        private const val EDIT_TEXT_VIEW_KEY = "EDIT_TEXT_VIEW_KEY"
+    }
+
+    private lateinit var searchEditText: EditText
+
+    private var savedValue: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        window.statusBarColor = MaterialColors.getColor(
+            findViewById<View>(android.R.id.content).rootView, R.attr.YP_White_to_YP_Black
+        )
+
+        savedValue = savedInstanceState?.getString(EDIT_TEXT_VIEW_KEY)
+
+        val backImage = findViewById<ImageView>(R.id.back_image)
+        backImage.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        searchEditText = findViewById(R.id.search_edit_text)
+        searchEditText.setText(savedValue)
+
+        val clearButton = findViewById<ImageView>(R.id.ic_clear_image_view)
+
+        clearButton.setOnClickListener {
+            savedValue = ""
+            searchEditText.setText(savedValue)
+            searchEditText.clearFocus()
+            hideDefaultKeyboard(searchEditText)
+        }
+
+        val textWatcherEditText = getTextWatcher(clearButton)
+
+        searchEditText.addTextChangedListener(textWatcherEditText)
+
+        val searchRV = findViewById<RecyclerView>(R.id.search_rv)
+        searchRV.adapter = SearchAdapter(getMultipleMockTrackList(100))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(EDIT_TEXT_VIEW_KEY, savedValue)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        savedValue = savedInstanceState.getString(EDIT_TEXT_VIEW_KEY)
+        searchEditText.setText(savedValue)
+    }
+
+    private fun isClearButtonVisible(s: CharSequence?): Boolean = !s.isNullOrEmpty()
+
+    private fun hideDefaultKeyboard(editText: EditText) {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(editText.windowToken, 0)
+    }
+
+    private fun getTextWatcher(clearButton: ImageView) = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            savedValue = s.toString()
+            Log.i(LOG_TAG, "Введеное значение: $savedValue")
+            clearButton.isVisible = isClearButtonVisible(s)
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    }
+
+    private fun getMultipleMockTrackList(n: Int): MutableList<Track> {
+        val result: MutableList<Track> = mutableListOf()
+        for (i in 1..n) {
+            result.addAll(getMockTrackList())
+        }
+        result.shuffle()
+        return result
+    }
+
+    private fun getMockTrackList() = listOf(
+        Track(
+            "Smells Like Teen Spirit",
+            "Nirvana",
+            "5:01",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "Michael Jackson",
+            "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "Bee Gees",
+            "4:10",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "Led Zeppelin",
+            "5:33",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "Guns N' Roses",
+            "5:03",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "No Reply",
+            "Guns N' Roses",
+            "5:03",
+            "/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+    )
+}
