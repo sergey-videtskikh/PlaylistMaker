ff8d118ffcbcdd449129b570e4b1bd819b2f22b0
Sergey Videtskikh <videtskikh@gmail.com>
2024-02-22 12:49:22 +0300

    Сдаём задачу спринта №10 на ревью (#4)
    * create search recycler view
    
    * correct the comments

--- app/src/main/java/ru/vsv/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/vsv/playlistmaker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.color.MaterialColors
+import ru.vsv.playlistmaker.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ru/vsv/playlistmaker/search/SearchAdapter.kt
@@ -0,0 +1,50 @@
+package ru.vsv.playlistmaker.search
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import ru.vsv.playlistmaker.R
+import ru.vsv.playlistmaker.dto.Track
+
+class SearchAdapter(private val dataSet: List<Track>) :
+    RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+
+    class SearchViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        private val albumCoverView: ImageView = view.findViewById(R.id.album_cover)
+        private val trackNameView: TextView = view.findViewById(R.id.track_name)
+        private val artistNameView: TextView = view.findViewById(R.id.artist_name)
+        private val trackTime: TextView = view.findViewById(R.id.track_time)
+
+        fun bind(track: Track) {
+            Glide.with(view)
+                .load(track.artworkUrl100)
+                .placeholder(R.drawable.search_placeholder)
+                .transform(CenterCrop(), RoundedCorners(2))
+                .into(albumCoverView)
+
+            trackNameView.text = track.trackName
+            artistNameView.text = track.artistName
+            trackTime.text = track.trackTime
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): SearchViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.search_item, viewGroup, false)
+
+        return SearchViewHolder(view)
+    }
+
+    override fun onBindViewHolder(searchViewHolder: SearchViewHolder, position: Int) {
+        searchViewHolder.bind(dataSet[position])
+    }
+
+    override fun getItemCount() = dataSet.size
+}
